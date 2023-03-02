46ce965a950e975f380429ab107e451c04515254
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-03-01 23:02:47 -0300

    firstCommit

---
+++ src/hooks/useAuthentication.jsx
@@ -0,0 +1,75 @@
+import { async } from '@firebase/util'
+import {db} from '../firebase/config'
+import {
+    getAuth, 
+    createUserWithEmailAndPassword,
+    singInUserWithEmailAndPassword,
+    updateProfile,
+    signOut
+} from 'firebase/auth'
+
+import { useState, useEffect } from 'react'
+
+export default function useAuthentication() {
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    //cleanup
+    const [cancelled, setCancelled] = useState(false)
+    
+    const auth = getAuth()
+
+    const checkIfisCancelled = () => {
+        if (cancelled) {
+            return
+        }
+    }
+
+    const createUser = async (data) => {
+        checkIfisCancelled()
+        setLoading(true)
+        setError(null)
+
+        try {
+            
+            const {user}  = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.pass,
+            )
+
+            await updateProfile(user, {
+                displayName: data.name
+            })
+            
+            setLoading(false)
+
+            return user
+
+        } catch (error) {
+            let systemErrorMessage
+
+            if (error.message.includes('password')) {
+                systemErrorMessage = 'A senha deve conter ao menos 6 caracteres'
+            } else if (error.message.includes('email-already')) {
+                systemErrorMessage = 'Email já cadastrado'
+            } else {
+                systemErrorMessage = 'Ocorreu um erro, por favor tente mais tarde'
+            }
+
+            setError(systemErrorMessage)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        return () => setCancelled()
+    }, [])
+
+    return {
+        auth, 
+        createUser, 
+        error, 
+        loading
+    }
+}
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,18 +1,55 @@
 import style from './Register.module.css'
 
-import { Link } from 'react-router-dom'
 
+import { Link } from 'react-router-dom'
 import {useState, useEffect} from 'react'
+import useAuthentication from '../../hooks/useAuthentication'
 
 //
 import iconPerson from '../assets/person.svg'
 import iconEmail from '../assets/envelope.svg'
 import iconPass from '../assets/key.svg'
+import iconError from '../assets/x.svg'
 
 export default function Register() {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+    const [confirmPass, setConfirmPass] = useState('')
+    const [error, setError] = useState('')
+
+    const {createUser, error: authError, loading} = useAuthentication()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        
+        setError('')
+        const user = {
+            name,
+            email,
+            pass
+        }
+
+        if (pass !== confirmPass) {
+            setError('As senhas não coincidem')
+            return
+        }
+
+        const res = await createUser(user)
+
+        console.log(user)
+    }
+
+    useEffect(() => {
+        if (authError) {
+            setError(authError)
+        }
+    }, [authError])
+
     return (
         <div>
-            <form className={style.form}>
+            <form onSubmit={handleSubmit} className={style.form}>
                 <h3>Cria sua conta Miniblog.</h3>
                 <div className={style.field}>
                     <img src={iconPerson} alt="Icone de usuário" className={style.icon}/>
@@ -22,7 +59,9 @@ export default function Register() {
                         name='name'
                         placeholder='Digite seu usuário'
                         required
-                        autoComplete='off'/>
+                        autoComplete='off'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}/>
                     </label>
                 </div>
                 <div className={style.field}>
@@ -33,7 +72,9 @@ export default function Register() {
                         name='email'
                         placeholder='Digite seu e-mail'
                         autoComplete='off'
-                        required/>
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}/>
                     </label>
                 </div>
                 <div className={style.field}>
@@ -43,7 +84,9 @@ export default function Register() {
                         type="password"
                         name='password'
                         placeholder='Digite sua senha'
-                        required/>
+                        required
+                        value={pass}
+                        onChange={(e) => setPass(e.target.value)}/>
                     </label>
                 </div>
                 <div className={style.field}>
@@ -53,10 +96,23 @@ export default function Register() {
                         type="password"
                         name='confirmPassword'
                         placeholder='Confirme a senha'
-                        required/>
+                        required
+                        value={confirmPass}
+                        onChange={(e) => setConfirmPass(e.target.value)}/>
                     </label>
                 </div>
-                <button className='button_primary'>Criar conta</button>
+                {error && 
+                    <span className={style.error} title='Informe a mesma senha em ambos os campos'>
+                        <img src={iconError} alt="" className={style.icon_error} />
+                        {error}
+                    </span>
+                }
+                {loading &&
+                    <button className='button_primary' title='Por favor, aguarde o carregamento' disabled>Criar conta</button>
+                }
+                {!loading &&
+                    <button className='button_primary'>Criar conta</button>
+                }
                 <Link to='/login' className='text-center'>
                     <span className='button_text'>Entrar na sua conta</span>
                 </Link>
