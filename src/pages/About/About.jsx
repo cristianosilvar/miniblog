6efc8185d4c1865b8e66bf09f48aef87b470a73d
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-03-03 12:17:36 -0300

    firstCommit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,10 @@
-import './About.module.css'
+import style from './About.module.css'
  
 export default function About() {
     return (
-        <div>
+        <div className='text-center'>
             <h1>Sobre</h1>
+            <p>Esse projeto consiste em um miniblog feito com React.js e Firebase.</p>
         </div>
     )
 }
